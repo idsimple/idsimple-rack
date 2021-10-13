c9401c6439b6abd0c565d2312bb61ddfe7ce95b8
Ari Summer <aribsummer@gmail.com>
2021-10-13 17:44:24 -0600

    Switch to rspec

--- Gemfile
+++ Gemfile
@@ -4,4 +4,7 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 12.0"
-gem "minitest", "~> 5.0"
+gem "rspec", "~> 3.0"
+gem "rack-test", "~> 1.1.0"
+gem "pry"
+gem "pry-byebug"
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require "idsimple/rack"
+require "rack/test"
+require "pry"
+
+RSpec.configure do |config|
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
