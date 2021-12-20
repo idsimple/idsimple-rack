3a1dd9637d6a620ed48618cfe3f655ece75ed129
Ari Summer <aribsummer@gmail.com>
2021-12-19 19:52:04 -0700

    Remove IP and user agent claim validation

--- lib/idsimple/rack/access_token_validator.rb
+++ lib/idsimple/rack/access_token_validator.rb
@@ -5,20 +5,9 @@ module Idsimple
     class AccessTokenValidator
       def self.validate_used_token_custom_claims(decoded_token, req)
         token_payload = decoded_token[0]
-        ip = token_payload["idsimple.ip"]
-        user_agent = token_payload["idsimple.user_agent"]
         used_at = token_payload["idsimple.used_at"]
 
         result = AccessTokenValidationResult.new
-
-        if ip && req.ip != ip
-          result.add_error("IP mismatch - Expected: #{ip} Actual: #{req.ip}")
-        end
-
-        if user_agent && req.user_agent != user_agent
-          result.add_error("User agent mismatch - Expected: #{user_agent} Actual: #{req.user_agent}")
-        end
-
         result.add_error("Missing used_at timestamp") if !used_at
         result.add_error("Invalid used_at timestamp") if used_at && used_at > Time.now.to_i
 
@@ -29,18 +18,8 @@ module Idsimple
         token_payload = decoded_token[0]
         use_by = token_payload["idsimple.use_by"]
         used_at = token_payload["idsimple.used_at"]
-        ip = token_payload["idsimple.ip"]
-        user_agent = token_payload["idsimple.user_agent"]
 
         result = AccessTokenValidationResult.new
-
-        if ip && req.ip != ip
-          result.add_error("IP mismatch - Expected: #{ip} Actual: #{req.ip}")
-        end
-
-        if user_agent && req.user_agent != user_agent
-          result.add_error("User agent mismatch - Expected: #{user_agent} Actual: #{req.user_agent}")
-        end
 
         if use_by && Time.now.to_i > use_by
           result.add_error("Token must be used prior to before claim")
--- spec/access_token_validator_spec.rb
+++ spec/access_token_validator_spec.rb
@@ -2,50 +2,6 @@ require "spec_helper"
 
 RSpec.describe Idsimple::Rack::AccessTokenValidator do
   describe ".validate_used_token_custom_claims" do
-    describe "validate IP claim" do
-      it "returns invalid result when IP mismatch" do
-        result = described_class.validate_used_token_custom_claims(
-          [{ "idsimple.ip" => "127.0.0.1", "idsimple.used_at" => Time.now.to_i }],
-          double(ip: "172.9.0.1")
-        )
-
-        expect(result.invalid?).to be true
-        expect(result.full_error_message).to include("IP mismatch")
-      end
-
-      it "returns valid result when IP matches" do
-        result = described_class.validate_used_token_custom_claims(
-          [{ "idsimple.ip" => "127.0.0.1", "idsimple.used_at" => Time.now.to_i }],
-          double(ip: "127.0.0.1")
-        )
-
-        expect(result.valid?).to be true
-        expect(result.full_error_message).to be_nil
-      end
-    end
-
-    describe "validate user agent claim" do
-      it "returns invalid result when user agent mismatch" do
-        result = described_class.validate_used_token_custom_claims(
-          [{ "idsimple.user_agent" => "Chrome", "idsimple.used_at" => Time.now.to_i }],
-          double(user_agent: "Safari")
-        )
-
-        expect(result.invalid?).to be true
-        expect(result.full_error_message).to include("User agent mismatch")
-      end
-
-      it "returns valid result when user agent matches" do
-        result = described_class.validate_used_token_custom_claims(
-          [{ "idsimple.user_agent" => "Chrome", "idsimple.used_at" => Time.now.to_i }],
-          double(user_agent: "Chrome")
-        )
-
-        expect(result.valid?).to be true
-        expect(result.full_error_message).to be_nil
-      end
-    end
-
     describe "validate used_at claim" do
       it "returns invalid result when missing used_at claim" do
         result = described_class.validate_used_token_custom_claims(
@@ -100,50 +56,6 @@ RSpec.describe Idsimple::Rack::AccessTokenValidator do
 
         expect(result.invalid?).to be true
         expect(result.full_error_message).to include("Token already used")
-      end
-    end
-
-    describe "validate IP claim" do
-      it "returns invalid result when IP mismatch" do
-        result = described_class.validate_unused_token_custom_claims(
-          [{ "idsimple.ip" => "127.0.0.1" }],
-          double(ip: "172.9.0.1")
-        )
-
-        expect(result.invalid?).to be true
-        expect(result.full_error_message).to include("IP mismatch")
-      end
-
-      it "returns valid result when IP matches" do
-        result = described_class.validate_unused_token_custom_claims(
-          [{ "idsimple.ip" => "127.0.0.1" }],
-          double(ip: "127.0.0.1")
-        )
-
-        expect(result.valid?).to be true
-        expect(result.full_error_message).to be_nil
-      end
-    end
-
-    describe "validate user agent claim" do
-      it "returns invalid result when user agent mismatch" do
-        result = described_class.validate_unused_token_custom_claims(
-          [{ "idsimple.user_agent" => "Chrome" }],
-          double(user_agent: "Safari")
-        )
-
-        expect(result.invalid?).to be true
-        expect(result.full_error_message).to include("User agent mismatch")
-      end
-
-      it "returns valid result when user agent matches" do
-        result = described_class.validate_unused_token_custom_claims(
-          [{ "idsimple.user_agent" => "Chrome" }],
-          double(user_agent: "Chrome")
-        )
-
-        expect(result.valid?).to be true
-        expect(result.full_error_message).to be_nil
       end
     end
   end
